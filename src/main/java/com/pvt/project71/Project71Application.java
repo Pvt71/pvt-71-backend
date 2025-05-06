65328397fa7baa430eebda18ed41328671df676e
Johannes N.K <natekjohannes@gmail.com>
2025-05-06 20:28:29 +0200

    H2 scope changed to test

--- src/main/java/com/pvt/project71/Project71Application.java
+++ src/main/java/com/pvt/project71/Project71Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableConfigurationProperties(RsaKeyProperties.class)
