643602d8b9902d142c0bb35e53ab3f94543add2e
Daniel Sandin <daniel.sandin2004@gmail.com>
2025-05-06 11:10:20 +0200

    Fixed application-test.properties files back to local db

--- src/test/java/com/pvt/project71/EventChallengeIntegrationTests.java
+++ src/test/java/com/pvt/project71/EventChallengeIntegrationTests.java
@@ -18,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -33,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class EventChallengeIntegrationTests {
     @Autowired
     private ChallengeService challengeService;
