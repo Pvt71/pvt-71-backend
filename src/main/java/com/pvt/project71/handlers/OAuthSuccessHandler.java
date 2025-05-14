cba19cdc33067c68c3031cc049d487e70fb77bb4
Daniel Sandin <daniel.sandin2004@gmail.com>
2025-05-14 11:11:58 +0200

    Fixed to 20 days token life

--- src/main/java/com/pvt/project71/handlers/OAuthSuccessHandler.java
+++ src/main/java/com/pvt/project71/handlers/OAuthSuccessHandler.java
@@ -32,9 +32,9 @@ public class OAuthSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
         OAuth2User oauthUser = (OAuth2User) authentication.getPrincipal();
-        String token = jwtService.generateToken(authentication, 1, ChronoUnit.MONTHS).getTokenValue();
+        String token = jwtService.generateToken(authentication, 20, ChronoUnit.DAYS).getTokenValue();
         //Send JWT back to OAuth2 process
-        objectMapper.writeValue(response.getWriter(), Map.of("token", token));
+        //objectMapper.writeValue(response.getWriter(), Map.of("token", token));
         String redirectPath = "/oauth-callback?token=" + URLEncoder.encode(token, "UTF-8");
 
         response.sendRedirect(redirectPath);
