8951964a6c2ef8d3a079c8ba2e21502dce522130
Askanberg <askanberg.git@gmail.com>
2025-05-16 11:22:14 +0200

    Adds webconfig for .well-known path structure

---
+++ src/main/java/com/pvt/project71/config/WebConfig.java
@@ -0,0 +1,17 @@
+
+package com.pvt.project71.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/.well-known/**")
+                .addResourceLocations("classpath:/static/.well-known/");
+    }
+}
