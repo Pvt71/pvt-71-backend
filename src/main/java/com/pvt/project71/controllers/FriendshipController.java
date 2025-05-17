b7d476fce156262fc575622116bccc5c0a17731b
PatrikSvensson <Patrik.Svensson02@gmail.com>
2025-05-17 11:48:49 +0200

    Added documentation to the friendship controller

--- src/main/java/com/pvt/project71/controllers/FriendshipController.java
+++ src/main/java/com/pvt/project71/controllers/FriendshipController.java
@@ -35,6 +35,21 @@ public class FriendshipController {
         this.userService = userService;
     }
 
+    /**
+     * {@code POST /friends/add/{email}} - Sends a new friend request.
+     * <p>Expects an email to the user you want to add, and valid JWT token.</p>
+     *
+     * @param receiverEmail the email of the user you want to send the request to
+     * @param requesterToken the JWT token of the authenticated user.
+     *
+     * @return ResponseEntity containing:
+     *      <ul>
+     *          <li>{@code 201 Created} and the friendship data in JSON format if the friend request is created.</li>
+     *          <li>{@code 400 Bad Request} if the users are already friends, or if you try to add yourself. </li>
+     *          <li>{@code 401 Unauthorized} if the JWT token is invalid. </li>
+     *          <li>{@code 404 Not Found} if the requester or receiver does not exist. </li>
+     *      <ul>
+     */
     @PostMapping(path = "/friends/add/{email}")
     public ResponseEntity<FriendshipDto> sendFriendRequest(@PathVariable("email") String receiverEmail,
                                                            @AuthenticationPrincipal Jwt requesterToken){
@@ -71,6 +86,21 @@ public class FriendshipController {
         return new ResponseEntity<>(friendshipMapper.mapTo(savedFriendship), HttpStatus.CREATED);
     }
 
+    /**
+     * {@code PUT /friends/accept/{email}} - Accepts a friend request.
+     * <p>Expects an email to the user who sent the request to you, and valid JWT token.</p>
+     *
+     * @param requesterEmail the email of the user who sent the request to you.
+     * @param receiverToken the JWT token of the authenticated user.
+     *
+     * @return ResponseEntity containing:
+     *      <ul>
+     *          <li>{@code 200 OK} and the friendship data in JSON format if the friend request is accepted.</li>
+     *          <li>{@code 400 Bad Request} if the user is receiver and requester, or if the friendship is not pending. </li>
+     *          <li>{@code 401 Unauthorized} if the JWT token is invalid. </li>
+     *          <li>{@code 404 Not Found} if the friend request is not found. </li>
+     *      <ul>
+     */
     @PutMapping(path = "/friends/accept/{email}")
     public ResponseEntity<FriendshipDto> acceptFriendRequest(@PathVariable("email") String requesterEmail,
                                                              @AuthenticationPrincipal Jwt receiverToken){
@@ -93,6 +123,18 @@ public class FriendshipController {
         return new ResponseEntity<>(friendshipMapper.mapTo(friendshipService.save(friendRequest)), HttpStatus.OK);
     }
 
+    /**
+     * {@code GET /friendrequests} - Returns a list of userDto in JSON format who has sent requests to the user.
+     * <p>Expects a valid JWT token.</p>
+     *
+     * @param userToken the JWT token of the authenticated user.
+     *
+     * @return ResponseEntity containing:
+     *      <ul>
+     *          <li>{@code 200 OK} and a list of UserDtos in JSON format.</li>
+     *          <li>{@code 401 Unauthorized} if the JWT token is invalid. </li>
+     *      <ul>
+     */
     @GetMapping(path = "/friendrequests")
     public ResponseEntity<List<UserDto>> getFriendRequests(@AuthenticationPrincipal Jwt userToken){
         if(userToken == null || !jwtService.isTokenValid(userToken)){
@@ -103,6 +145,18 @@ public class FriendshipController {
         return new ResponseEntity<>(friends, HttpStatus.OK);
     }
 
+    /**
+     * {@code GET /friends} - Returns a list of userDto in JSON format who are friends with the user.
+     * <p>Expects a valid JWT token.</p>
+     *
+     * @param userToken the JWT token of the authenticated user.
+     *
+     * @return ResponseEntity containing:
+     *      <ul>
+     *          <li>{@code 200 OK} and a list of UserDtos in JSON format.</li>
+     *          <li>{@code 401 Unauthorized} if the JWT token is invalid. </li>
+     *      <ul>
+     */
     @GetMapping(path = "/friends")
     public ResponseEntity<List<UserDto>> getFriends(@AuthenticationPrincipal Jwt userToken){
         if(userToken == null || !jwtService.isTokenValid(userToken)){
@@ -113,7 +167,20 @@ public class FriendshipController {
         return new ResponseEntity<>(friends, HttpStatus.OK);
     }
 
-    // Reject requests and remove friendships
+    /**
+     * {@code DELETE /friends/{email}} - Used for both deleting friendships and rejecting friend requests.
+     * <p>Expects an email to the other user, and a valid JWT token.</p>
+     *
+     * @param otherUsersEmail the email of the other user.
+     * @param userToken the JWT token of the authenticated user.
+     *
+     * @return ResponseEntity containing:
+     *      <ul>
+     *          <li>{@code 204 No Content} if the friendship is deleted.</li>
+     *          <li>{@code 401 Unauthorized} if the JWT token is invalid. </li>
+     *          <li>{@code 404 Not Found} if the friendship or friend request does not exist.</li>
+     *      <ul>
+     */
     @DeleteMapping(path = "/friends/{email}")
     public ResponseEntity<FriendshipDto> deleteFriendship(@PathVariable("email") String otherUsersEmail,
                                                           @AuthenticationPrincipal Jwt userToken){
